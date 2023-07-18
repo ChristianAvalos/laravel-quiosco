cb0ab179227b803e700951b940cad7e907ce7df4
Christian <christiand.avalos.e@gmail.com>
2023-07-17 21:52:11 -0400

    creando el inicio de sesion y el registro con reac

--- react-quiosco/src/layaouts/AuthLayout.jsx
+++ react-quiosco/src/layaouts/AuthLayout.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom"
 export default function AuthLayout() {
     return (
-        <main>
-            <img src="../" alt="imagen logotipo" />
+        <main className="max-w-4xl m-auto mt-10 md:mt-28 flex flex-col md:flex-row items-center">
+            <img src="../img/logo.svg" 
+            alt="imagen logotipo" 
+            className="max-w-xs"
+            />
 
-            <div>
+            <div className="p-10 w-full">
                 <Outlet/>
             </div>
             
