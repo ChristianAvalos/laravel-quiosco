2abbd585b7a58ea2371ed35bec05050d810ff6a1
Christian <christiand.avalos.e@gmail.com>
2023-07-17 21:07:57 -0400

    cambio de lugar img

--- react-quiosco/src/layaouts/AuthLayout.jsx
+++ react-quiosco/src/layaouts/AuthLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom"
 export default function AuthLayout() {
     return (
         <main>
-            <img src="" alt="imagen logotipo" />
+            <img src="../" alt="imagen logotipo" />
 
             <div>
                 <Outlet/>
