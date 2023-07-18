7405c920f1fcfbd9f823978ecd127f69c32dc8d6
Christian <christiand.avalos.e@gmail.com>
2023-07-17 21:01:27 -0400

    creando rutas y vistas en react

--- react-quiosco/src/router.jsx
+++ react-quiosco/src/router.jsx
@@ -1,11 +1,36 @@
 import { createBrowserRouter }from 'react-router-dom'
 import Layout from './layaouts/Layout'
+import Inicio from './views/Inicio'
+import AuthLayout from './layaouts/AuthLayout'
+import Login from './views/Login'
+import Registro from './views/Registro'
 
 const router = createBrowserRouter([
     {
         path:'/',
-        element: <Layout />
-    }
+        element: <Layout/>,
+        children:[
+            {
+                index: true,
+                element: <Inicio/>,
+            }
+        ]
+
+    },
+    {
+        path: '/auth',
+        element: <AuthLayout/>,
+        children:[
+            {
+                path:'/auth/login',
+                element:<Login/>
+            },
+            {
+                path:'/auth/registro',
+                element:<Registro/>
+            }
+        ]
+    },
 ])
 
 export default router
---
+++ react-quiosco/src/views/Inicio.jsx
@@ -0,0 +1,5 @@
+export default function Inicio() {
+    return (
+        <div>Inicio ese es mi hijo</div>
+    )
+}
