beef4cb62e8ed5f2040af8190fa8959f875b2d6a
Christian <christiand.avalos.e@gmail.com>
2023-07-18 22:23:57 -0400

    creando el contenido

--- react-quiosco/src/components/Sidebar.jsx
+++ react-quiosco/src/components/Sidebar.jsx
@@ -15,10 +15,24 @@ export default function Sidebar() {
         <div className="mt-10">
             {categorias.map( categoria =>(
                 <Categoria
+                    key={categoria.id}
                     categoria={categoria}
                 />
             ))}
         </div>
+
+        <div className="my-5 px-5">
+            <button
+                type="button"
+                className="text-center bg-red-500 w-full p-3 font-bold text-white truncate"
+            >
+                Cancelar Orden
+            </button>
+            
+        </div>
+
+
+
         </div>
     )
 }
